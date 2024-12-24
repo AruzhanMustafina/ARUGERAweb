2efd199d3bf4456a7ad751792a187202796c5b78
AruzhanMustafina <amira.lin.250502@gmail.com>
2024-12-24 18:36:19 +0500

    Update server.js

--- server.js
+++ server.js
@@ -4,15 +4,12 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// Устанавливаем статическую папку для сервирования файлов
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Главный маршрут
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Запуск сервера
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
